f94422ac6d4929e7db6c2b8e27c1ab0515cc7f15
Guillermo D'Angelo <dangeloguillermo@gmail.com>
2022-01-27 11:42:24 -0300

    ch

--- app.py
+++ app.py
@@ -24,28 +24,51 @@ def cargar_dd_deptos():
     dd_deptos['largo_limite_km'] = dd_deptos.largo_limite/1000
 
     return dd_deptos
-    
+
+
 dd = cargar_dd_deptos()
 
 prodSim = sm.load('data/restringido_origen_mvo.pickle')
 
 nom_depto = [
-    'Montevideo', 'Artigas', 'Canelones',
-    'Cerro Largo', 'Colonia', 'Durazno',
-    'Flores', 'Florida', 'Lavalleja',
-    'Maldonado', 'Paysandú', 'Río Negro',
-    'Rivera', 'Rocha', 'Salto', 'San José',
-    'Soriano', 'Tacuarembó', 'Treinta y Tres'
+    'Montevideo',
+    'Artigas',
+    'Canelones',
+    'Cerro Largo',
+    'Colonia',
+    'Durazno',
+    'Flores',
+    'Florida',
+    'Lavalleja',
+    'Maldonado',
+    'Paysandú',
+    'Río Negro',
+    'Rivera',
+    'Rocha',
+    'Salto',
+    'San José',
+    'Soriano',
+    'Tacuarembó',
+    'Treinta y Tres'
     ]
-
 
 factor_edad = st.slider('Aumento edad', 0.0, 5.0, value=1.2)
 
-dd['log_edad_promedio_des'] = np.log(factor_edad * dd.edad_prom_des)
+factor_pbi = st.slider('Aumento PBI', 0.0, 5.0, value=1.2)
 
-st.dataframe(dd['log_edad_promedio_des'])
 
+
+dd_new = dd.copy()
+
+dd_new['log_edad_promedio_des'] = np.log(factor_edad * dd_new.edad_prom_des)
+st.write(dd_new['log_edad_promedio_des'])
 st.write(prodSim.summary())
+
+
+dd_new['log_pbi_destino_millardos'] = np.log(factor_pbi * dd_new.pbi_destino_millardos)
+st.write(dd_new['log_pbi_destino_millardos'])
+st.write(prodSim.summary())
+
 
 def print_scores_simple(ground_truth, estimation):
     "Imprime r cuadrado y error mínimo cuadrático de un modelo"
@@ -57,13 +80,13 @@ def print_scores_simple(ground_truth, estimation):
 
 
 actual_counts = dd.personas_mig
-new_pred = prodSim.predict(dd)
+new_pred = prodSim.predict(dd_new)
 
 #predicted_counts = new_pred.summary_frame()['mean'].round(0).astype(int)
 
-st.dataframe(actual_counts)
+st.write(actual_counts)
 
-st.dataframe(new_pred)
+st.write(new_pred)
 
 
 print_scores_simple(actual_counts, new_pred)
