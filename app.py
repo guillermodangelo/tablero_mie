580372eb90cad291df6cc22839cab3de5b26d4d7
Guillermo D'Angelo <dangeloguillermo@gmail.com>
2022-01-27 10:53:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 ﻿import streamlit as st
 import pandas as pd
+import numpy as np
 import statsmodels.api as sm
+from sklearn.metrics import r2_score, mean_squared_error
 
 st.set_page_config(page_title='Test')
 
@@ -16,7 +18,6 @@ según los datos del Censo INE 2011.
 
 st.caption(desc)
 
-@st.cache(persist=True)
 def cargar_dd_deptos():
     dd_deptos = pd.read_csv('data/dd_deptos.csv', sep=';', decimal=',')
     dd_deptos.loc[dd_deptos.largo_limite.isna(), 'largo_limite'] = 0.0001
@@ -26,7 +27,7 @@ def cargar_dd_deptos():
     
 dd = cargar_dd_deptos()
 
-#prodSim = sm.load('data/restringido_origen_mvo.pickle')
+prodSim = sm.load('data/restringido_origen_mvo.pickle')
 
 nom_depto = [
     'Montevideo', 'Artigas', 'Canelones',
@@ -38,6 +39,35 @@ nom_depto = [
     ]
 
 
-edad = st.slider('Aumento edad', 0.0, 5.0, value=1.2)
+factor_edad = st.slider('Aumento edad', 0.0, 5.0, value=1.2)
 
-st.write(edad)
+dd['log_edad_promedio_des'] = np.log(factor_edad * dd.edad_prom_des)
+
+st.dataframe(dd['log_edad_promedio_des'])
+
+st.write(prodSim.summary())
+
+def print_scores_simple(ground_truth, estimation):
+    "Imprime r cuadrado y error mínimo cuadrático de un modelo"
+    r2 = r2_score(ground_truth, estimation)
+    rmse = mean_squared_error(ground_truth, estimation, squared=False)
+    
+    st.write("R² = " + round(r2, 4).astype(str))
+    st.write("RMSE = " + round(rmse, 4).astype(str))
+
+
+actual_counts = dd.personas_mig
+new_pred = prodSim.predict(dd)
+
+#predicted_counts = new_pred.summary_frame()['mean'].round(0).astype(int)
+
+st.dataframe(actual_counts)
+
+st.dataframe(new_pred)
+
+
+print_scores_simple(actual_counts, new_pred)
+
+title = 'Groud truth vs. estimaciones'
+subtitle = 'MIE restringido en origen con Mvdeo.'
+
